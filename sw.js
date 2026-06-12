c296855e464e6037188521ff4b72320cd636d6ab
jranners <julius.rummelsdorf@gmail.com>
2026-06-12 15:17:43 +0200

    deploy: cache kstw-logo.png in service worker for offline support

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const STATIC_ASSETS = [
   './index.html',
   './app.js',
   './manifest.json',
+  './kstw-logo.png',
   'https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;600;700&display=swap',
   'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap',
   'https://cdn.tailwindcss.com?plugins=forms,container-queries'
